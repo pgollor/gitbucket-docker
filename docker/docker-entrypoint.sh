046e13bee2ba72d33c0c2e9b73296f227ff21cf8
Pascal Gollor <kalle@pgollor.de>
2017-05-27 18:28:32 +0200

    fix execute bug

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # replace mysql settings in config file
-dbfile=${GITBUCKET_HOME}/database.config
+dbfile=${GITBUCKET_HOME}/database.conf
 sed -i "/GITBUCKET_DATABASE_NAME/c\\${GITBUCKET_DATABASE_NAME};" $dbfile
 sed -i "/GITBUCKET_DATABASE_HOST/c\\${GITBUCKET_DATABASE_HOST};" $dbfile
 sed -i "/GITBUCKET_DATABASE_PASSWORD/c\\${GITBUCKET_DATABASE_PASSWORD};" $dbfile
 sed -i "/GITBUCKET_DATABASE_USER/c\\${GITBUCKET_DATABASE_USER};" $dbfile
 
-
-# start gitbucket
