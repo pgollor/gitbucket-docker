ac7f2ae74b19d406ced8391e7668b86c99009515
Pascal Gollor <kalle@pgollor.de>
2017-06-02 17:17:38 +0200

    bind data and gist dir

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+
 # backup directory
 backupDir=./backup
 mkdir -p ${backupDir}
 mkdir -p ${backupDir}/db
+if [ -d "data/data" ]; then
+	mkdir -p ${backupDir}/data
+fi
+if [ -d "data/gist" ]; then
+	mkdir -p ${backupDir}/gist
+fi
 mkdir -p ${backupDir}/repositories
 
 # current date
@@ -19,6 +26,18 @@ rm ${dbFile}
 # backup repositories
 repoFile="${backupDir}/repositories/${currentDate}.tbz2"
 tar -cj data/repositories -f ${repoFile}
+
+# backup gist repositories if existing
+if [ -d "data/gist" ]; then
+	repoFile="${backupDir}/gist/${currentDate}.tbz2"
+	tar -cj data/gist -f ${repoFile}
+fi
+
+# backup data if existing
+if [ -d "data/data" ]; then
+	repoFile="${backupDir}/data/${currentDate}.tbz2"
+	tar -cj data/data -f ${repoFile}
+fi
 
 # delete all files older 10 days
 find ${backupDir} -iname "*.tbz2" -type f -mtime +10 -exec rm {} \; > /dev/null
